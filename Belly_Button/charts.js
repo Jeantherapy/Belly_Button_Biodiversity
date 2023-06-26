657aedb9be1b7015fe3618278e18dbd9f0ee70de
Jeantherapy <121070752+Jeantherapy@users.noreply.github.com>
2023-06-26 11:09:57 -0400

    fil update

--- Belly_Button/charts.js
+++ Belly_Button/charts.js
@@ -37,7 +37,6 @@ function buildMetadata(sample) {
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
     var result = resultArray[0];
-   
     // Use d3 to select the panel with id of `#sample-metadata`
     var PANEL = d3.select("#sample-metadata");
 
@@ -54,60 +53,44 @@ function buildMetadata(sample) {
   });
 }
 
-// Deliverable 1: 1. Create the buildChart function.
+// 1. Create the buildCharts function.
 function buildCharts(sample) {
-  // Deliverable 1: 2. Use d3.json to load the samples.json file 
+  // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
-    console.log(data);
-
-    // Deliverable 1: 3. Create a variable that holds the samples array. 
+    // 3. Create a variable that holds the samples array. 
     console.log(data);
     var samplesArray = data.samples;
     console.log(samplesArray);
-    // Deliverable 1: 4. Create a variable that filters the samples for the object with the desired sample number.
-    var resultArray = samplesArray.filter(sampleObj => sampleObj.id === sample);
-    console.log(resultArray);
-    // Deliverable 3: 1. Create a variable that filters the metadata array for the object with the desired sample number.
-    var metadataArray = data.metadata;
-    var resultMetadata = metadataArray.filter(sampleObj => sampleObj.id === sample);
-    console.log(resultMetadata);
-
-    // Deliverable 1: 5. Create a variable that holds the first sample in the array.
-    var firstSample = resultArray[0];
+    // 4. Create a variable that filters the samples for the object with the desired sample number.
+    var selectedIdSamples = samplesArray.filter(data => data.id == sample);
+    console.log(selectedIdSamples);
+    //  5. Create a variable that holds the first sample in the array.
+    var firstSample = selectedIdSamples[0];
     console.log(firstSample);
-    // Deliverable 3: 2. Create a variable that holds the first sample in the metadata array.
-    var firstMetadata = resultMetadata[0];
-    console.log(firstMetadata);
-    // Deliverable 1: 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
+
+    // 6. Create variables that hold the otu_ids, otu_labels, and sample_values.
     var otuIds = firstSample.otu_ids;
     var otuLabels = firstSample.otu_labels;
     var sampleValues = firstSample.sample_values;
     console.log(otuIds);
     console.log(otuLabels);
+    //console.log("hello");
     console.log(sampleValues);
-    // Deliverable 3: 3. Create a variable that holds the washing frequency.
-    var wFreq = parseFloat(firstMetadata.wfreq);
 
-    // Deliverable 1: 7. Create the yticks for the bar chart.
-    // Hint: Get the the top 10 otu_ids and map them in descending order 
-    // so the otu_ids with the most bacteria are last. 
-     var yticks = otuIds.slice(0,10).map(id => "OTU " + id).reverse();
+    // 7. Create the yticks for the bar chart.
+    // Hint: Get the the top 10 otu_ids and map them in descending order  
+    //  so the otu_ids with the most bacteria are last. 
+
+    var yticks = otuIds.slice(0,10).map(id => "OTU " + id).reverse();
     console.log(yticks);
-
-    // Deliverable 1: 8. Create the trace for the bar chart. 
-     var barData = [{
-      x: sampleValues.slice(0, 10).reverse(),
-      y: yticks,
-      text: otuLabels.slice(0, 10).reverse(),
-      type: "bar",
-      orientation: "h",
-      marker: {
-        color: sampleValues.slice(0, 10).reverse(),
-        colorscale: "Electric"
-      }
+    
+    // 8. Create the trace for the bar chart. 
+    var barData = [{
+      x: sampleValues.slice(0,10).reverse(),
+      text: otuLabels.slice(0,10).reverse(),
+      type: "bar"
     }];
-
-    // Deliverable 1: 9. Create the layout for the bar chart. 
+    // 9. Create the layout for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacteria Cultures Found",
       yaxis: {
@@ -126,10 +109,17 @@ function buildCharts(sample) {
         showarrow: false
       }]
     };
+    //console.log("hello");
+    // 10. Use Plotly to plot the data with the layout. 
+    Plotly.newPlot("bar", barData, barLayout, {responsive: true});
 
-    // Deliverable 1: 10. Use Plotly to plot the data with the layout. 
-     Plotly.newPlot("bar", barData, barLayout, {responsive: true});
-    // Deliverable 2: 1. Create the trace for the bubble chart.
+    // Bar and Bubble charts
+    // Create the buildCharts function.
+    //function buildCharts(sample) {
+    // Use d3.json to load and retrieve the samples.json file 
+    //d3.json("samples.json").then((data) => {
+
+    // 1. Create the trace for the bubble chart.
     var bubbleData = [{
       x: otuIds,
       y: sampleValues,
@@ -142,7 +132,7 @@ function buildCharts(sample) {
       }
     }];
     console.log(bubbleData);
-    // Deliverable 2: 2. Create the layout for the bubble chart.
+    // 2. Create the layout for the bubble chart.
     var bubbleLayout = {
       title: 'Bacteria Cultures Per Sample',
       showlegend: false,
@@ -153,10 +143,17 @@ function buildCharts(sample) {
     };
     console.log(bubbleLayout);
 
-    // Deliverable 2: 3. Use Plotly to plot the data with the layout.
+    // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot("bubble", bubbleData, bubbleLayout, {responsive: true});
 
-    // Deliverable 3: 4. Create the trace for the gauge chart.
+    // 1. Create a variable that filters the metadata array for the object with the desired sample number.
+    var metadata_SelId = data.metadata.filter(data => data.id == sample);
+    console.log(metadata_SelId);  
+
+    // 3. Create a variable that holds the washing frequency.
+    var washFreq = +metadata_SelId[0].wfreq;
+    
+    // 4. Create the trace for the gauge chart.
     var gaugeData = [
       {
         domain: { x: [0, 1], y: [0, 1] },
@@ -181,7 +178,8 @@ function buildCharts(sample) {
         }
       }
     ];
-    // Deliverable 3: 5. Create the layout for the gauge chart.
+    
+    // 5. Create the layout for the gauge chart.
     var gaugeLayout = { 
       autosize: true,
       annotations: [{
@@ -196,7 +194,7 @@ function buildCharts(sample) {
       }]
     };
 
-    // Deliverable 3: 6. Use Plotly to plot the gauge data and layout.
-     Plotly.newPlot("gauge", gaugeData, gaugeLayout, {responsive: true});
+    // 6. Use Plotly to plot the gauge data and layout.
+    Plotly.newPlot("gauge", gaugeData, gaugeLayout, {responsive: true});
   });
 }
--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # Belly_Button_Biodiversity
+**Belly Button Biodiversity Dashboard**
+
+**Introduction**
+The Belly Button Biodiversity Dashboard is an interactive web application that allows users to explore and analyze data related to the microbial biodiversity found in human belly buttons. This project aims to provide a visually appealing and user-friendly interface to delve into the fascinating world of microbial communities residing in our navels.
+
+**Data Source**
+The data used in the Belly Button Biodiversity Dashboard is sourced from a dataset called "samples.json." This dataset contains information about various test subjects and their corresponding microbial samples collected from their belly buttons. It includes demographic information, sample metadata, and microbial species data.
+
+**Dashboard Features**
+The Belly Button Biodiversity Dashboard offers several features for data exploration and visualization:
+
+1. Test Subject Selection: The dashboard provides a dropdown menu to select a specific test subject ID. Choosing a subject triggers the update of all charts and metadata information based on the selected ID.
+
+2. Demographic Information: The dashboard displays demographic information for the selected test subject, including age, gender, ethnicity, and location.
+
+3. Horizontal Bar Chart: The bar chart showcases the top 10 microbial species found in the selected test subject's belly button. The chart represents the sample values (abundance) of each species, with the species labels displayed on the y-axis and the sample values on the x-axis.
+
+4. Bubble Chart: The bubble chart visualizes the distribution of microbial species across the selected test subject's belly button sample. Each bubble represents a specific microbial species, and its size corresponds to its abundance. The x-axis represents the OTU (Operational Taxonomic Unit) ID, and the y-axis represents the sample values.
+
+5. Gauge Chart: The gauge chart displays the belly button washing frequency of the selected test subject. It represents the number of times the individual cleans their belly button per week using a gauge-like visualization.
+
+**Customization and Extension**
+The Belly Button Biodiversity Dashboard can be customized and extended in several ways:
+
+1. Styling: The dashboard's appearance can be modified by changing the CSS styles, including fonts, colors, backgrounds, and layout.
+
+2. Additional Charts: Users can enhance the dashboard by adding more visualizations, such as pie charts, line charts, or heatmaps, to provide further insights into the microbial data.
+
+3. Interactive Elements: The dashboard can be expanded to include additional interactive elements, such as filters or sliders, to allow users to explore the data from different perspectives or focus on specific subsets of the dataset.
+
+**Usage**
+To use the Belly Button Biodiversity Dashboard, follow these steps:
+
+1. Open the "index.html" file in a web browser.
+2. Use the dropdown menu to select a test subject ID.
+3. Explore the demographic information, bar chart, bubble chart, and gauge chart for the selected test subject.
+4. Customize the dashboard, charts, or styling as desired by modifying the "charts.js" and "index.html" files.
+
+**Conclusion**
+The Belly Button Biodiversity Dashboard provides a captivating and informative interface for exploring and understanding the microbial biodiversity present in human belly buttons. With its intuitive charts and interactive features, users can gain insights into the complex microbial communities that inhabit our navels. By customizing and extending the dashboard, it can be adapted to accommodate further research, education, or data analysis requirements related to belly button biodiversity.
